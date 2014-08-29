286b6f634a13e8dbced474059729ecf6205c64f3
Niels van Mourik <niels.vanmourik@acquia.com>
2014-08-29 22:33:51 +0200

    NEW hook_acquia_purge_variations_alter() added, before anyone would file issues for it.
    function hook_acquia_purge_variations_alter($path, &$variations) {
      if (in_array($path, array('<front>', '', '/'))) {
        $variations[] = 'rss.xml';
      }
      if ($path === 'news') {
        $variations[] = 'news/feed';
      }
    }
    
    $ drush ap-purge 'news <front>'
    Purged: http://mysite.com/news
    Purged: http://mysite.com/news/
    Purged: http://mysite.com/news/feed
    Purged: http://mysite.com/
    Purged: http://mysite.com/node
    Purged: http://mysite.com/node/
    Purged: http://mysite.com/rss.xml

--- acquia_purge.api.php
+++ acquia_purge.api.php
@@ -43,5 +43,36 @@ function hook_acquia_purge_domains_alter(&$domains) {
 }
 
 /**
+ * Edit/extend the list of variations for $path.
+ *
+ * When the site has variations enabled, administrators will automatically
+ * see their manual purge attempts getting more purged than they asked for,
+ * which happens to aid them in wiping absolutely everything they wanted
+ * cleaned. For instance editors on your site always want to get the RSS
+ * feed refreshed when they wipe the 'news' page, which you can easily
+ * tweak for them with this hook.
+ *
+ * @param string $path
+ *   The Drupal path (for example: '<front>', 'user/1' or a alias).
+ * @param $variations
+ *   All the variations that have been made up as possible other incarnations
+ *   of the page that needs a manual wipe. You can delete items as well as
+ *   adding new ones, as long as they are path sections (and NOT full urls!)
+ *   on which Acquia Purge can perform Varnish purges thereafter.
+ *
+ * @see _acquia_purge_input_path_variations()
+ * @return
+ *   Void.
+ */
+function hook_acquia_purge_variations_alter($path, &$variations) {
+  if (in_array($path, array('<front>', '', '/'))) {
+    $variations[] = 'rss.xml';
+  }
+  if ($path === 'news') {
+    $variations[] = 'news/feed';
+  }
+}
+
+/**
  * @} End of "addtogroup hooks".
  */
