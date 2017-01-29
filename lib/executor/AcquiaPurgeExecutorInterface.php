9c13cc2d7bf9e8afe84932d7ea05b4dfa6622d74
Niels van Mourik <niels.vanmourik@acquia.com>
2017-01-29 16:54:50 +0100

    executor

--- lib/executor/AcquiaPurgeExecutorInterface.php
+++ lib/executor/AcquiaPurgeExecutorInterface.php
@@ -6,21 +6,73 @@
  */
 
 /**
- * Describes a executor, which executes a set of path purges.
+ * Describes an executor, which executes a set of path purges.
  */
 interface AcquiaPurgeExecutorInterface {
 
   /**
-   * Determine if the processor considers itself enabled.
-   */
+  * Determine if the executor is enabled or not.
+  */
   public static function isEnabled();
 
   /**
-   * Subscribe to the events this processor requires.
+   * Construct a executor object.
    *
-   * @return string[]
-   *   Non-associative array of event names.
+   * @param AcquiaPurgeService $service
+   *   The Acquia Purge service instance.
    */
-  public function getSubscribedEvents();
+  public function __construct(AcquiaPurgeService $service);
+
+  /**
+   * Claims multiple items from the queue for processing.
+   *
+   * @param string $scheme
+   *   The requested scheme to be invalidated: 'http' or 'https'.
+   * @param string $domain
+   *   The domain name to clear the path on, e.g. "foo.com" or "bar.baz".
+   * @param string $path
+   *   The path to wipe, e.g. 'user/1?destination=foo' or 'news/*'.
+   *
+   * @return array
+   *   On success we return a non-associative array with item objects. When the
+   *   queue has no items that can be claimed, this doesn't return FALSE as
+   *   claimItem() does, but an empty array instead.
+   *
+   *   If claims return, the objects have at least these properties:
+   *   - data: the same as what what passed into createItem().
+   *   - item_id: the unique ID returned from createItem().
+   *   - created: timestamp when the item was put into the queue.
+   */
+  public function getRequests($scheme, $domain, $path);
+
+  /**
+   * Claims multiple items from the queue for processing.
+   *
+   * @param int $claims
+   *   Determines how many claims at once should be claimed from the queue. When
+   *   the queue is unable to return as many items as requested it will return
+   *   as much items as it can.
+   * @param int $lease_time
+   *   How long the processing is expected to take in seconds, defaults to an
+   *   hour. After this lease expires, the item will be reset and another
+   *   consumer can claim the item. For idempotent tasks (which can be run
+   *   multiple times without side effects), shorter lease times would result
+   *   in lower latency in case a consumer fails. For tasks that should not be
+   *   run more than once (non-idempotent), a larger lease time will make it
+   *   more rare for a given task to run multiple times in cases of failure,
+   *   at the cost of higher latency.
+   *
+   * @return array
+   *   On success we return a non-associative array with item objects. When the
+   *   queue has no items that can be claimed, this doesn't return FALSE as
+   *   claimItem() does, but an empty array instead.
+   *
+   *   If claims return, the objects have at least these properties:
+   *   - data: the same as what what passed into createItem().
+   *   - item_id: the unique ID returned from createItem().
+   *   - created: timestamp when the item was put into the queue.
+   */
+  public function evaluate($request);
+
 
 }
--- lib/executor/AcquiaPurgeExecutorsService.php
+++ lib/executor/AcquiaPurgeExecutorsService.php
@@ -15,7 +15,10 @@ class AcquiaPurgeExecutorsService {
    *
    * @var string[]
    */
-  protected $backends = array();
+  protected $backends = array(
+    'executor_ah',
+    'executor_pagecache',
+  );
 
   /**
    * The loaded backends.
@@ -23,15 +26,6 @@ class AcquiaPurgeExecutorsService {
    * @var AcquiaPurgeExecutorInterface
    */
   protected $executors = array();
-
-  ---
-
-  /**
-   * Event-processor mapping.
-   *
-   * @var AcquiaPurgeProcessorInterface[]
-   */
-  protected $events = array();
 
   /**
    * The Acquia Purge service object.
@@ -41,7 +35,7 @@ class AcquiaPurgeExecutorsService {
   protected $service;
 
   /**
-   * Construct a new AcquiaPurgeProcessorsService instance.
+   * Construct a new AcquiaPurgeExecutorsService instance.
    *
    * @param AcquiaPurgeService $service
    *   The Acquia Purge service object.
@@ -50,75 +44,15 @@ class AcquiaPurgeExecutorsService {
     $this->service = $service;
 
     // Make sure the base class is available in case it gets referenced.
-    _acquia_purge_load('processor_base');
+    _acquia_purge_load('executor_base');
 
-    // Initialize the processors that advertize themselves as enabled.
+    // Initialize the executors that advertize themselves as enabled.
     foreach ($this->backends as $service) {
       $class = _acquia_purge_load($service);
       if ($class::isEnabled()) {
-        $this->processors[$class] = new $class($this->service);
+        $this->executors[$class] = new $class($this->service);
       }
     }
-
-    // Query the enabled processors for their subscribed events.
-    foreach ($this->processors as $processor) {
-      foreach ($processor->getSubscribedEvents() as $event) {
-        if (!isset($this->events[$event])) {
-          $this->events[$event] = array();
-        }
-        $this->events[$event][] = $processor;
-      }
-    }
-  }
-
-  /**
-   * Emit a particular event.
-   *
-   * @param string $event
-   *   Name of the event, often derived from hook implementations found directly
-   *   in acquia_purge.module, but could also be 'onItemsQueued'.
-   * @param mixed $a1
-   *   First optional parameter to pass by reference.
-   * @param mixed $a2
-   *   Second optional parameter to pass by reference.
-   */
-  public function emit($event, &$a1 = NULL, &$a2 = NULL) {
-
-    // Don't emit when there are no subscribers.
-    if (!isset($this->events[$event])) {
-      return;
-    }
-
-    // Emit to subscribed processors and pass on any given parameters.
-    if (isset($this->events[$event])) {
-      foreach ($this->events[$event] as $processor) {
-        if (is_null($a1)) {
-          $processor->$event();
-        }
-        elseif (is_null($a2)) {
-          $processor->$event($a1);
-        }
-        else {
-          $processor->$event($a1, $a2);
-        }
-      }
-    }
-  }
-
-  /**
-   * Retrieve a loaded processor object.
-   *
-   * @param string $class
-   *   The name of the processor class you need.
-   *
-   * @return AcquiaPurgeProcessorInterface|false
-   *   Returns the processor, or FALSE when it doesn't exist.
-   */
-  public function get($class) {
-    if (!isset($this->processors[$class])) {
-      return FALSE;
-    }
-    return $this->processors[$class];
   }
 
 }
