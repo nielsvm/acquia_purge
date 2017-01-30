fee85abe54692caa4cd03ad5dd620e168813c4d3
Niels van Mourik <niels.vanmourik@acquia.com>
2017-01-30 14:27:08 +0100

    AcquiaPurgeExecutorsService is now iterable and working.

--- lib/executor/AcquiaPurgeExecutorsService.php
+++ lib/executor/AcquiaPurgeExecutorsService.php
@@ -8,16 +8,16 @@
 /**
  * Service that loads and provides access to executor backends.
  */
-class AcquiaPurgeExecutorsService {
+class AcquiaPurgeExecutorsService implements \Iterator {
 
   /**
-   * The _acquia_purge_load() services in load and execution order.
+   * Core executor backends that always run regardless what.
    *
    * @var string[]
    */
-  protected $backends = array(
-    '_acquia_purge_executor_ah',
+  protected $core_backends = array(
     '_acquia_purge_executor_page_cache',
+    '_acquia_purge_executor_ah',
   );
 
   /**
@@ -26,6 +26,13 @@ class AcquiaPurgeExecutorsService {
    * @var AcquiaPurgeExecutorInterface
    */
   protected $executors = array();
+
+  /**
+   * Current iterator position.
+   *
+   * @var int
+   */
+  protected $position = 0;
 
   /**
    * The Acquia Purge service object.
@@ -44,14 +51,69 @@ class AcquiaPurgeExecutorsService {
     $this->service = $service;
     _acquia_purge_load('_acquia_purge_executor_interface');
     _acquia_purge_load('_acquia_purge_executor_base');
-
-    // Initialize the executors that advertize themselves as enabled.
-    foreach ($this->backends as $service) {
-      $class = _acquia_purge_load($service);
+    foreach ($this->getRegisteredBackends() as $service_or_path) {
+      $class = _acquia_purge_load($service_or_path);
       if ($class::isEnabled()) {
-        $this->executors[$class] = new $class($this->service);
+        $instance = new $class($this->service);
+        if (!($instance instanceof AcquiaPurgeExecutorInterface)) {
+          throw new \RuntimeException("$class != AcquiaPurgeExecutorInterface");
+        }
+        $this->executors[] = $instance;
       }
     }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function current() {
+    if ($this->valid()) {
+      return $this->executors[$this->position];
+    }
+    return FALSE;
+  }
+
+  /**
+   * Retrieve all loadable backends.
+   *
+   * @return string[]
+   *   List of paths or _acquia_purge_load() service names.
+   */
+  protected function getRegisteredBackends() {
+    $external_backends = array();
+    foreach (module_implements('acquia_purge_executors') as $module) {
+      $function = $module . '_acquia_purge_executors';
+      $function($external_backends);
+    }
+    return array_merge($this->core_backends, $external_backends);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function key() {
+    return $this->position;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function next() {
+    ++$this->position;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function rewind() {
+    $this->position = 0;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function valid() {
+    return isset($this->executors[$this->position]);
   }
 
 }
