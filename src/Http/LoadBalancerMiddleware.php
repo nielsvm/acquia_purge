5edf2b21eef4fe49fd07e36bfdd1caa3aed2fe1f
Niels van Mourik <niels.vanmourik@acquia.com>
2017-07-23 14:07:13 +0200

    [GUZZLE] Hardened LoadBalancerMiddleware by catching 403's, 405's and other METHODs.

--- src/Http/LoadBalancerMiddleware.php
+++ src/Http/LoadBalancerMiddleware.php
@@ -27,37 +27,43 @@ class LoadBalancerMiddleware {
         // Return a handler that throws exceptions on bad responses.
         return $handler($request, $options)->then(
           function (ResponseInterface $response) use ($request, $handler, $options) {
+            $status = $response->getStatusCode();
             $method = $request->getMethod();
 
-            // PURGE requests should return either a 200 or a 404.
-            if ($method == 'PURGE') {
-              if (!in_array($response->getStatusCode(), [200, 404])) {
-                throw new FailedInvalidationException(
-                  sprintf(
-                    "%s expected 200 or 404 but got %s!",
-                    $request->getMethod() . ' ' . $request->getUri(),
-                    $response->getStatusCode()
-                  ),
-                  $request,
-                  $response
-                );
-              }
+            // Define a tiny closure that throws exceptions for us.
+            $e = function($msg) use ($request, $response, $method, $status) {
+              $msg = sprintf("HTTP %d; %s %s; %s", $status, $method, $request->getUri(), $msg);
+              throw new FailedInvalidationException($msg, $request, $response);
+            };
+
+            // Flag up suspicious response types.
+            if ($status === 403) {
+              $e('Forbidden is abnormal and suggests that your balancer runs'
+              . ' on a malfunctioning custom VCL configuration!');
+            }
+            elseif ($status == 405) {
+              $e('Not allowed; Chances are that you customized the VCL file'
+              . ' running on your balancer for a customized cache invalidation'
+              . ' token. Please contact Acquia Support and consider reverting'
+              . ' your configuration as these setups are no longer supported by'
+              . ' the acquia_purge module.');
             }
 
-            // BAN requests should always return a 200.
-            elseif ($method == 'BAN') {
-              if ($response->getStatusCode() !== 200) {
-                throw new FailedInvalidationException(
-                  sprintf(
-                    "%s expected 200 but got %s!",
-                    $request->getMethod() . ' ' . $request->getUri(),
-                    $response->getStatusCode()
-                  ),
-                  $request,
-                  $response
-                );
+            // Test response codes per HTTP method.
+            if ($method == 'PURGE') {
+              if (!in_array($status, [200, 404])) {
+                $e("Expected 200 or 404 instead!");
               }
             }
+            elseif ($method == 'BAN') {
+              if ($status !== 200) {
+                $e("Expected 200 instead!");
+              }
+            }
+            else {
+              $e("Unsupported HTTP method!");
+            }
+
             return $response;
           }
         );
