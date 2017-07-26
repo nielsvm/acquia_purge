c6099917bbc58eefa3bfd4d64a9eee284da3a655
Niels van Mourik <niels.vanmourik@acquia.com>
2017-07-26 09:23:45 +0200

    MERGED AcquiaCloudCheck into AcquiaPurgeCheck to reduce overhead and present a clearer, single diagnostic to the end user (checking many things).

--- src/Plugin/Purge/DiagnosticCheck/AcquiaPurgeCheck.php
+++ src/Plugin/Purge/DiagnosticCheck/AcquiaPurgeCheck.php
@@ -6,6 +6,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\purge\Plugin\Purge\DiagnosticCheck\DiagnosticCheckInterface;
 use Drupal\purge\Plugin\Purge\DiagnosticCheck\DiagnosticCheckBase;
 use Drupal\purge\Plugin\Purge\Purger\PurgersServiceInterface;
+use Drupal\acquia_purge\HostingInfoInterface;
 
 /**
  * Acquia Purge.
@@ -13,12 +14,17 @@ use Drupal\purge\Plugin\Purge\Purger\PurgersServiceInterface;
  * @PurgeDiagnosticCheck(
  *   id = "acquia_purge",
  *   title = @Translation("Acquia Purge"),
- *   description = @Translation("Reports on the Acquia Purge module."),
+ *   description = @Translation("Reports the status of the Acquia Purge module."),
  *   dependent_queue_plugins = {},
  *   dependent_purger_plugins = {}
  * )
  */
 class AcquiaPurgeCheck extends DiagnosticCheckBase implements DiagnosticCheckInterface {
+
+  /**
+   * @var \Drupal\acquia_purge\HostingInfoInterface
+   */
+  protected $acquiaPurgeHostinginfo;
 
   /**
    * @var \Drupal\purge\Plugin\Purge\Purger\PurgersServiceInterface
@@ -30,6 +36,8 @@ class AcquiaPurgeCheck extends DiagnosticCheckBase implements DiagnosticCheckInt
    *
    * @param \Drupal\purge\Plugin\Purge\Purger\PurgersServiceInterface $purge_purgers
    *   The purge purgers service.
+   * @param \Drupal\acquia_purge\HostingInfoInterface $acquia_purge_hostinginfo
+   *   Technical information accessors for the Acquia Cloud environment.
    * @param array $configuration
    *   A configuration array containing information about the plugin instance.
    * @param string $plugin_id
@@ -37,8 +45,9 @@ class AcquiaPurgeCheck extends DiagnosticCheckBase implements DiagnosticCheckInt
    * @param mixed $plugin_definition
    *   The plugin implementation definition.
    */
-  public function __construct(PurgersServiceInterface $purge_purgers, array $configuration, $plugin_id, $plugin_definition) {
+  public function __construct(PurgersServiceInterface $purge_purgers, HostingInfoInterface $acquia_purge_hostinginfo, array $configuration, $plugin_id, $plugin_definition) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->acquiaPurgeHostingInfo = $acquia_purge_hostinginfo;
     $this->purgePurgers = $purge_purgers;
   }
 
@@ -48,6 +57,7 @@ class AcquiaPurgeCheck extends DiagnosticCheckBase implements DiagnosticCheckInt
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
     return new static(
       $container->get('purge.purgers'),
+      $container->get('acquia_purge.hostinginfo'),
       $configuration,
       $plugin_id,
       $plugin_definition
@@ -58,16 +68,32 @@ class AcquiaPurgeCheck extends DiagnosticCheckBase implements DiagnosticCheckInt
    * {@inheritdoc}
    */
   public function run() {
+    $version = system_get_info('module', 'acquia_purge')['version'];
+    $version = is_null($version) ? '8.x-1.x-dev' : $version;
+    $this->value = $version;
 
-    // Report when the Acquia Purge purger isn't added.
-    if (in_array('acquia_purge', $this->purgePurgers->getPluginsAvailable())) {
-      $this->recommendation = $this->t("Please add the 'Acquia Cloud' purger!");
+    // Block the entire system when this is a third-party platform.
+    if (!$this->acquiaPurgeHostingInfo->isThisAcquiaCloud()) {
+      $this->recommendation = $this->t("Acquia Purge only works on your Acquia Cloud environment and doesn't work outside of it.");
+      return SELF::SEVERITY_ERROR;
+    }
+
+    // Issue a warning when the user forgot to add the AcquiaCloudPurger.
+    if (!in_array('acquia_purge', $this->purgePurgers->getPluginsEnabled())) {
+      $this->recommendation = $this->t("The 'Acquia Cloud' purger is not installed!");
       return SELF::SEVERITY_WARNING;
     }
 
-    // Report the module version.
-    $v = system_get_info('module', 'acquia_purge')['version'];
-    $this->recommendation = is_null($v) ? $this->t("Development version.") : $v;
+    // Under normal operating conditions, we'll report site info and version.
+    $this->value = $this->t(
+      "@site_group.@site_env (@version)",
+      [
+        '@site_group' => $this->acquiaPurgeHostingInfo->getSiteGroup(),
+        '@site_env' => $this->acquiaPurgeHostingInfo->getSiteEnvironment(),
+        '@version' => $version,
+      ]
+    );
+    $this->recommendation = '';
     return SELF::SEVERITY_OK;
   }
 
